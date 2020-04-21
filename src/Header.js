36d369e6db711bc7d5c90a20927610cd9f25ac36
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-21 07:40:49 +0100

    First Commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1>Music Junkie</h1>
+            <img src={require('./images/dark-rider-JmVaNyemtN8-unsplash.jpg')}></img>
+        </header>
+    )
+}
+
+export default Header;
