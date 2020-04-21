f638281597b3ff04a5c5396c8a31d5628aa62574
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-21 07:55:59 +0100

    Updates

---
+++ precache-manifest.892d68752c64a273163e1803833e44dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3427f71a99e6cc61fb5e4f5dc362f407",
+    "url": "/Music_App/index.html"
+  },
+  {
+    "revision": "5b65287e0a5815d91f7c",
+    "url": "/Music_App/static/css/main.009bff14.chunk.css"
+  },
+  {
+    "revision": "eb8931f70d305bb04b7b",
+    "url": "/Music_App/static/js/2.7e5a4f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Music_App/static/js/2.7e5a4f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b65287e0a5815d91f7c",
+    "url": "/Music_App/static/js/main.a1628a76.chunk.js"
+  },
+  {
+    "revision": "c2206bf8f773a55616e1",
+    "url": "/Music_App/static/js/runtime-main.8656e4a0.js"
+  },
+  {
+    "revision": "ad8348e951cb12c0b6208b4537626b42",
+    "url": "/Music_App/static/media/dark-rider-JmVaNyemtN8-unsplash.ad8348e9.jpg"
+  }
+]);
